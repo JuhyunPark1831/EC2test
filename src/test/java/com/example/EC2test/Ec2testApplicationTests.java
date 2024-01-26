8d5871863c50bd7a47c86125cb2f30d828564715
JuhyunPark1831 <epahs1831@naver.com>
2024-01-26 16:41:35 +0900

    Feat: First commit

---
+++ src/test/java/com/example/EC2test/Ec2testApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.EC2test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ec2testApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
